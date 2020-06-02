09f3822776892573c7924f44e061b9e1ecf971c6
unknown <rashadbayar@gmail.com>
2020-06-01 22:47:51 -0700

    update landing page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-   console.log("hello"); 
 
+//typeWriter effect
 const texts = ['Developer', 'Creator'];
 let count = 0;
 let index = 0; 
@@ -19,7 +19,7 @@ let letter = "";
         count++;
         index = 0; 
     }
-    setTimeout(type, 400)
+    setTimeout(type, 500)
 
 }());
 
@@ -28,71 +28,3 @@ let letter = "";
 
 
 
-
-   /* const TypeWriter = function(txtElement, words, wait = 3000) {
-       this.txtElement = txtElement
-       this.words = words;
-       this.txt = "";
-       this.wordIndex = 0;
-       this.wait = parseInt(wait, 10);
-       this.type();
-       this.isDeleting = false;
-   }
-
-   TypeWriter.prototype.type = function() {
-    const current = this.wordIndex % this.words.length;
-
-    const fullTxt = this.words[current]; 
-
-    //check if isDeleting
-    if(this.isDeleting) {
-        //remove a character
-        this.txt = fullTxt.substring(0, this.txt.length + 1)
-    }else {
-        //add a character
-        this.txt = fullTxt.substring(0, this.txt.length + 1)
-
-    }
-
-    //inset into element 
-    this.txtElement.innerHtML = `<span class="txt">${this.txt}</span>`; 
-
-    //type speed 
-    let typeSpeed = 300; 
-
-    if(this.isDeleting) {
-        typeSpeed /= 2; 
-
-    }
-
-    //if words is complete
-    if(!this.isDeleting && this.txt === fullTxt) {
-        typeSpeed = this.wait; 
-        //set delete is true 
-        this.isDeleting = true; 
-
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false
-
-        this.wordIndex++;
-        //pause before start typing 
-        typeSpeed = 500; 
-    }
-
-
-    
-
-       setTimeout(() => this.type(), 500);
-   }
-
-    document.addEventListener('DOMcontentLoaded', init);  
-
-   function init() {
-       const txtElement = document.getElementById('#txt-type');
-       const words = JSON.parse(txtElement.getAttribute('data-words')); 
-       const wait = txtElement.getAttribute('data-wait'); 
-       new TypeWriter(txtElement, words, wait);
-
-
-    
-   } */
